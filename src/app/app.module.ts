39ed411b7c4cb4f4729e9a6ae5df845d81bdd888
Karol Kepa <kkepa@deloittece.com>
2022-09-29 10:50:35 +0200

    feat: working login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginAndRegisterComponent } from './login-and-register/login-and-register.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginAndRegisterComponent,
     LoginComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
