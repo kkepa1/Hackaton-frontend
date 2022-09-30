89faa492d091cdfcb0a93c49fec531c5d06d5816
Karol Kepa <kkepa@deloittece.com>
2022-09-30 14:38:19 +0200

    feat: działa wszystko

--- src/app/components/cake/cake.component.ts
+++ src/app/components/cake/cake.component.ts
@@ -13,42 +13,6 @@ export class CakeComponent implements OnInit {
 
   public posts: Observable<Post[]> = this.postService.getAllPosts()
 
-  // public posts: Post[] = [{
-  //   username: 'username',
-  //   description: 'PLACEK DROŻDŻOWY ZE ŚLIWKAMI I CZEKOLADĄ',
-  //   cakeImageSource: "../../assets/images/ciasto1.jpg",
-  //   likes: 4,
-  //   comments: 1,
-  //   dateOfPublication: '30.09.2022',
-  //   listOfComments: [
-  //     {
-  //      commentUser: 'superuser123',
-  //      commentText: 'to nawet nie jest on',
-  //      commentDate: '30.09.2022'
-  //     },]
-  // },
-  //   {
-  //     username: 'inny_użytkownik',
-  //     description: 'ciasto dobre',
-  //     cakeImageSource: "../../assets/images/ciasto2.jpg",
-  //     likes: 10,
-  //     comments: 2,
-  //     dateOfPublication: '29.09.2022',
-  //     listOfComments: [
-  //       {
-  //         commentUser: 'superuser123',
-  //         commentText: 'dobre',
-  //         commentDate: '29.09.2022'
-  //       },
-  //       {
-  //         commentUser: 'username',
-  //         commentText: 'bardzo dobre',
-  //         commentDate: '30.09.2022'
-  //       },
-  //
-  // ]}
-  //   ]
-
   constructor( public postService: PostService) { }
 
   ngOnInit(): void {
@@ -58,7 +22,7 @@ export class CakeComponent implements OnInit {
   likePost(id: number): void{
     this.postService.likePost(id).subscribe(res => console.log(res));
   }
-  addComment(): void{
+  addComment(postId: number): void{
     // this.posts[0].comments =+ 1
     // this.posts[0].listOfComments.push(
     //   {commentUser: LocalService.getLoggedUser(),
